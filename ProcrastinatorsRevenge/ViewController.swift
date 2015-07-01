2f8602d2882bb53777609b6e04f91f37fba0b179
Lyndsey Scott <lyndseyscott@Lyndseys-MacBook-Air.local>
2015-07-01 10:51:52 -0700

    Initial Commit

---
+++ ProcrastinatorsRevenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProcrastinatorsRevenge
+//
+//  Created by Lyndsey Scott on 7/1/15.
+//  Copyright (c) 2015 Standable Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
